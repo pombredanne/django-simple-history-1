bc83c2fb4a631a2f422213bb7b91293b9ea38cea
Steven Klass <sklass@pivotalenergysolutions.com>
2013-07-12 20:58:21 -0700

    Using __version__ as defined in the package.

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@
 """setup.py: Django django-simple-history"""
 
 from setuptools import find_packages, setup
+from simple_history import __version__
+
+base_url = 'https://github.com/pivotal-energy-solutions/django-simple-history'
 
 setup(name='simple_history',
-      version='v1.2.6',
+      version='v{0}'.format(__version__),
       description='Store Django model history with the ability to revert back to a '
                   'specific change at any time. This includes capturing request.user',
       author='Steven Klass',
       author_email='sklass@pivotalenergysolutions.com',
-      url='https://github.com/pivotal-energy-solutions/django-simple-history',
-      download_url='https://github.com/pivotal-energy-solutions/django-simple-history/releases/tag/v1.2.6',
+      url=base_url,
+      download_url='{0}/releases/tag/v{}'.format(base_url, __version__),
       license='Apache License (2.0)',
       classifiers=[
              'Development Status :: 2 - Pre-Alpha',
---
+++ simple_history/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Steven Klass'
+__version_info__ = (1, 2, 7)
+__version__ = '.'.join(map(str, __version_info__))
+__date__ = '2012/3/5 4:47:00 PM'
+__copyright__ = 'Copyright 2012-2013 Pivotal Energy Solutions. All rights reserved.'
+__credits__ = ['Steven Klass', 'Tim Valenta', 'Rajesh Pappula', 'Mohammad Rafi', 'Amit Kumar Pathak']
+__license__ = 'See the file LICENSE.txt for licensing information.'
