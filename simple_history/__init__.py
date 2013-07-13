dd580890f2be951bb8eac77c77526467e630559c
Steven Klass <sklass@pivotalenergysolutions.com>
2013-07-12 21:16:04 -0700

    Final stuff - this should work..

--- simple_history/__init__.py
+++ simple_history/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Steven Klass'
-__version_info__ = (1, 2, 8)
-__version__ = '.'.join(map(str, __version_info__))
-__date__ = '2012/3/5 4:47:00 PM'
-__copyright__ = 'Copyright 2012-2013 Pivotal Energy Solutions. All rights reserved.'
-__credits__ = ['Steven Klass', 'Tim Valenta', 'Rajesh Pappula', 'Mohammad Rafi', 'Amit Kumar Pathak']
+__version_info__ = (1, 2, 9)
+__version__ = 'v' + '.'.join(map(str, __version_info__))
+__date__ = '2010/10/22 4:47:00 PM'
+__credits__ = ['Steven Klass', 'Marty Alchin', 'George Vilches', 'Corey Betram']
 __license__ = 'See the file LICENSE.txt for licensing information.'
