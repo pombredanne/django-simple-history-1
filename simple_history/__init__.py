2209d06039f8c014837674747b006ffaa73b6999
Steven Klass <sklass@pivotalenergysolutions.com>
2013-07-12 22:56:03 -0700

    Let's try this..

--- simple_history/__init__.py
+++ simple_history/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Steven Klass'
-__version_info__ = (1, 3, 4)
-__version__ = 'v' + '.'.join(map(str, __version_info__))
+__version_info__ = (1, 3, 5)
+__version__ = '.'.join(map(str, __version_info__))
 __date__ = '2010/10/22 4:47:00 PM'
 __credits__ = ['Steven Klass', 'Marty Alchin', 'George Vilches', 'Corey Betram']
 __license__ = 'See the file LICENSE.txt for licensing information.'
